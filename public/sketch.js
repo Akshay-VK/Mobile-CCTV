90275a3fdf73dbfd0c5656ca50bed532f33ce5ee
Akshay VK <73469520+Akshay-VK@users.noreply.github.com>
2021-01-15 09:13:22 +0530

    initial commit with the first code added!

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,13 @@
+var capture;
+
 function setup() {
 	createCanvas(500,500);
+	capture = createCapture(VIDEO);
+	capture.size(500,500);
+	capture.hide();
 }
 
 function draw() {
 	background(0);
+	image(capture,0,0,500,500);
 }
