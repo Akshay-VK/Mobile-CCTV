1e93b8523ee3dc7d3b7165e0a5e01096d0d59855
Akshay VK <73469520+Akshay-VK@users.noreply.github.com>
2021-01-15 08:54:37 +0530

    First Commit!!

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.static('public'));
+
+app.listen(port , () => {
+	console.log("Server started at port "+port);
+});
