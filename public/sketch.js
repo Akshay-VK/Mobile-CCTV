19fb59ec3d2adca2a03207b3d03d9d6d0ac2138b
Akshay VK <73469520+Akshay-VK@users.noreply.github.com>
2021-01-17 11:08:37 +0530

    ok, many changes done.I added the new client and setting route..im not sure if the set route will work..hope it does

--- public/sketch.js
+++ public/sketch.js
@@ -1,13 +1,42 @@
 var capture;
+let ID;
+let fc = 0;
 
 function setup() {
 	createCanvas(500,500);
 	capture = createCapture(VIDEO);
-	capture.size(500,500);
+	capture.size(5,5);
 	capture.hide();
+
+	loadJSON('/new',(data) => {
+		ID = parseInt(data.id);
+	})
 }
 
 function draw() {
 	background(0);
-	image(capture,0,0,500,500);
+	scale(10);
+	fc++;
+	image(capture,0,0,50,50);
+	scale(0.1);
+	loadPixels();
+
+	let url = '/set';
+	for(let x = 0; x < width; x++) {
+		for(let y = 0; y < height; y++) {
+			var index = 4*(y*width+x);
+			if(index == 0) {
+				url = url + '?arr='+pixels[index];
+			}else{
+				url = url + '&arr='+pixels[index];
+			}
+		}
+	}
+	if(fc % 30 == 0){
+		loadJSON(url, (data) => {
+			console.log('SUCCESS');
+		});
+	}
+
+	updatePixels();
 }
--- server.js
+++ server.js
@@ -2,7 +2,25 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
+let videos = [];
+
 app.use(express.static('public'));
+
+let counter = 0;
+
+app.get('/new',(req,res) => {
+	res.send({id:counter});
+	counter++;
+});
+
+app.get('/set',(req,res) => {
+	let videofeed = req.params.arr;
+	for(let i = 0;i < videofeed.length; i++) {
+		videofeed[i] = parseInt(videofeed[i]);
+	}
+	videos.push(videofeed);
+	console.log(videos);
+});
 
 app.listen(port , () => {
 	console.log("Server started at port "+port);
